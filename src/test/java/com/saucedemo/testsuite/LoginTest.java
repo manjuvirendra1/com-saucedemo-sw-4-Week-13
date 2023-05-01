5e177e1edccd03dc360586992683c3fb78f19335
manjuvirendra1 <mvthakur@gmail.com>
2023-05-01 23:16:44 +0100

    My homework week 13

---
+++ src/test/java/com/saucedemo/testsuite/LoginTest.java
@@ -0,0 +1,34 @@
+package com.saucedemo.testsuite;
+
+import com.saucedemo.pages.HomePage;
+import com.saucedemo.pages.Products;
+import com.saucedemo.testbase.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class LoginTest extends BaseTest {
+    HomePage homePage = new HomePage();
+    Products products = new Products();
+
+
+    @Test
+    public void userShouldLoginSuccessfullyWithValidCredentials() {
+        homePage.enterUserName("standard_user");
+        homePage.enterpassword("secret_sauce");
+        homePage.clickOnLoginButton();
+        String actualMessage = products.verifyText();
+        String expectedMessage = "Products";
+        Assert.assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    public void verifyThatSixProductsAreDisplayedOnPage() {
+        homePage.enterUserName("standard_user");
+        homePage.enterpassword("secret_sauce");
+        homePage.clickOnLoginButton();
+        int actualNumber = products.verifyProductList();
+        int expectedNumber = 6;
+        Assert.assertEquals(actualNumber,expectedNumber);
+    }
+}
